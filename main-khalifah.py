8183529d3fb3bf408b30e0e8ead2ea1e685dc189
Khalifah Ayu <vanesaberliana16@student.uns.ac.id>
2026-05-04 19:06:58 +0700

    Update main-khalifah.py
    revisi main khalifah

--- main-khalifah.py
+++ main-khalifah.py
@@ -5,26 +5,21 @@ from pemeriksaan import Pemeriksaan
 from hewan import Hewan
 
 def main():
-    # 1. Inisialisasi Objek
-    pemilik1 = Pemilik("Aulia", "Surakarta", "082145678901")
+    pemilik1 = Pemilik("Khalifah Ayu", "Surakarta", "082145678901")
     hewan1 = Hewan("Mochi", "Kucing", 2, "Demam dan tidak nafsu makan")
     dokter1 = Dokter("Dr. Salsabila", "D002", "Hewan Kecil", 120000)
     ruang1 = Ruang("R02", "Ruang Kucing", 4)
 
-    # 2. Relasi Pemilik dan Hewan
     pemilik1.tambah_hewan(hewan1)
 
-    # 3. Testing Skenario 1: Pemeriksaan TANPA rawat inap
     print("\n[TEST SKENARIO 1: RAWAT JALAN]")
     periksa1 = Pemeriksaan(hewan1, dokter1)
     periksa1.tampilkan_detail()
 
-    # 4. Testing Skenario 2: Pemeriksaan DENGAN rawat inap
     print("\n[TEST SKENARIO 2: RAWAT INAP]")
     periksa2 = Pemeriksaan(hewan1, dokter1, ruang1, 3)
     periksa2.tampilkan_detail()
 
-    # 5. Menampilkan Data Master
     print("\n[DATA MASTER]")
     pemilik1.tampilkan_data()
     hewan1.tampilkan_info()
